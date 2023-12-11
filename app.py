fc40dc450f4aa793a493268e21ac1a3b28b28075
Sibasis555 <sahoosibasis555@gmail.com>
2023-12-11 16:35:39 +0530

    updated app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def img_emotion():
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-	return 'Hello World!!'
+	return 'Hello World!!?'
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=8000)
