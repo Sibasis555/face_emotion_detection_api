56eabb99cde27f039a2566613b040d5864c45f36
Sibasis555 <sahoosibasis555@gmail.com>
2023-12-12 16:37:41 +0530

    added debug

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def check():
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-	return 'Hello World!!?'
+	return 'Hello World!!!'
 
 if __name__=='__main__':
     app.run(debug=True)
