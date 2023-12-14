6945b785d992529f8ceb338ae593b727a6c7efc4
Sibasis555 <sahoosibasis555@gmail.com>
2023-12-14 12:45:02 +0530

    debug mode updated

--- app.py
+++ app.py
@@ -128,5 +128,5 @@ def hello_world():
 	return 'Hello Sibasis!!!'
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
     # app.run(host='0.0.0.0', port=8000)
